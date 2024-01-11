4b4cc1a323f93debc0b9552d5e48a01d377ee20c
Mekhi Mcfowler <mmcfowler@brooklynsteamcenter.org>
2024-01-11 10:44:14 -0500

    hui

--- todo.py
+++ todo.py
@@ -1,7 +1,26 @@
 from flask import Flask, render_template, request, redirect
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 import pymysql
 import pymysql.cursors
 from pprint import pprint as print
+
+app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+users = {
+    "mmcfowler": generate_password_hash("2208781851"),
+    "mekhi": generate_password_hash("220878151"),
+    "kai": generate_password_hash("220878151"),
+
+}
+
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and check_password_hash(users.get(username), password):
+        return username
+
 
 connection = pymysql.connect(
     host="10.100.33.60",
@@ -12,10 +31,9 @@ connection = pymysql.connect(
     cursorclass=pymysql.cursors.DictCursor,
 )
 
-app = Flask(__name__)
-
 
 @app.route("/", methods=["GET", "POST"])
+@auth.login_required
 def index():
     if request.method == "POST":
         new_todo = request.form.get("new_todo")
@@ -45,14 +63,14 @@ def todo_delete(todo_id):
 @app.route("/complete_todo/<int:todo_id>", methods=["POST"])
 def todo_complete(todo_id):
     cursor = connection.cursor()
-    
     cursor.execute(f"SELECT `completion` FROM `todos` WHERE `id` = {todo_id}")
-    current_completion = cursor.fetchone()['completion']
-
+    current_completion = cursor.fetchone()["completion"]
     new_completion = 1 if current_completion == 0 else 0
-
     cursor.execute(f"UPDATE `todos` SET `completion` = {new_completion} WHERE `id` = {todo_id}")
     connection.commit()
     cursor.close()
-    
     return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run()
